1144c712982303134af2a5b7fc529c128337ffa8
Hamilton Yobo <programmyobo@gmail.com>
2025-05-10 19:38:31 +0100

    Desabilitado opção DEBUG no settings

--- dev/settings.py
+++ dev/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e00t&orfs9isz&-pd^8&(v71tl=(uubyg9rfmqk56w*3yx62w%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mestre-doc.onrender.com', 'mestredoc.com']
 
